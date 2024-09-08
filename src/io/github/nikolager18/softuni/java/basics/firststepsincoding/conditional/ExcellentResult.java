88dc164e5be5f2c1208e677b399e7f5e24c94710
NikolaGer18 <nikola.gerdjikov7@abv.bg>
2024-09-08 12:00:53 +0300

    Conditional code added

---
+++ src/io/github/nikolager18/softuni/java/basics/firststepsincoding/conditional/ExcellentResult.java
@@ -0,0 +1,18 @@
+package io.github.nikolager18.softuni.java.basics.firststepsincoding.conditional;
+
+import java.util.Scanner;
+
+public class ExcellentResult {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int grade =Integer.parseInt(scan.nextLine());
+        if(grade >= 5)
+        {
+            System.out.println("Excellent!");
+        }
+        else if (grade >= 6)
+        {
+            System.out.println("Excellent!");
+        }
+    }
+}
